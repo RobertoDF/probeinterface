063557466bad122f33dc2340d3fbaa1d4e843a48
Roberto <37729096+RobertoDF@users.noreply.github.com>
2026-05-20 17:36:48 +0200

    add "return None" after checking probe detected

--- src/probeinterface/io.py
+++ src/probeinterface/io.py
@@ -771,7 +771,8 @@ def read_spikegadgets(file: str | Path, raise_error: bool = True) -> ProbeGroup:
     if n_probes == 0:
         if raise_error:
             raise Exception("No supported Neuropixels probes found")
-
+        return None
+         
     probe_group = ProbeGroup()
 
     for curr_probe_idx, probe_config in enumerate(probe_configs):
